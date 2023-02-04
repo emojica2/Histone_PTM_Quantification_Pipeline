26b69a990f766d86786e5dcdb9ce4e9c525e70f2
Lizzy Jarett <eajarett@ucdavis.edu>
2023-02-03 22:15:39 -0800

    Updated Skyline Export

---
+++ code/Printing_Plots.R
@@ -0,0 +1,17 @@
+# Printing plots based on the code in the Rmd file "Tilapia_Tissue_Analysis"
+
+print(PCA_Plot)
+
+print(H1_like_9589_map)
+print(H2A_8939_map)
+print(H2B_1_2_map)
+print(H3_map)
+print(H4_like_map)
+
+print(panel_volcano_plot_H1)
+
+print(Top_hPTMs_Plot_H1)
+print(Top_hPTMs_Plot_H2A)
+print(Top_hPTMs_Plot_H2B)
+print(Top_hPTMs_Plot_H3)
+print(Top_hPTMs_Plot_H4)
