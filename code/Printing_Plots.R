4f3ed03fe381b8346567b8f6d4dcc096ac83ee90
emojica2 <eajarett@ucdavis.edu>
2023-02-04 14:38:53 -0800

    Printing plots

--- code/Printing_Plots.R
+++ code/Printing_Plots.R
@@ -1,17 +1,11 @@
 # Printing plots based on the code in the Rmd file "Tilapia_Tissue_Analysis"
 
+print(Digestion_Barchart)
+
+print(panel_Histone_H1) # saved as pdf 10.41" x 9.5"
+print(panel_Histone_H2A) # saved as pdf 13.7" x 13"
+print(panel_Histone_H2B) # saved as pdf 12.5" x 8.29"
+print(panel_Histone_H3) # saved as pdf 12" x 11.5"
+print(panel_Histone_H4) # saved as pdf 10.16" x 8.05"
+
 print(PCA_Plot)
-
-print(H1_like_9589_map)
-print(H2A_8939_map)
-print(H2B_1_2_map)
-print(H3_map)
-print(H4_like_map)
-
-print(panel_volcano_plot_H1)
-
-print(Top_hPTMs_Plot_H1)
-print(Top_hPTMs_Plot_H2A)
-print(Top_hPTMs_Plot_H2B)
-print(Top_hPTMs_Plot_H3)
-print(Top_hPTMs_Plot_H4)
