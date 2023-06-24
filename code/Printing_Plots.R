8dc6a5017416e34123ab0a0c8498cd7bf8dd9603
Lizzy Jarett <eajarett@ucdavis.edu>
2023-06-24 15:55:53 -0700

    PTM AScores in analysis

--- code/Printing_Plots.R
+++ code/Printing_Plots.R
@@ -2,10 +2,10 @@
 
 print(Digestion_Barchart)
 
-print(panel_Histone_H1) # saved as pdf 10.41" x 9.5"
-print(panel_Histone_H2A) # saved as pdf 13.7" x 13"
-print(panel_Histone_H2B) # saved as pdf 12.5" x 8.29"
-print(panel_Histone_H3) # saved as pdf 12" x 11.5"
-print(panel_Histone_H4) # saved as pdf 10.16" x 8.05"
+print(panel_Histone_H1) # saved as pdf 12.02" x 7.47"
+print(panel_Histone_H2A) # saved as pdf 14.00" x 7.63"
+print(panel_Histone_H2B) # saved as pdf 11.11" x 7.63"
+print(panel_Histone_H3) # saved as pdf 11.11" x 8.05"
+print(panel_Histone_H4) # saved as pdf 8.43" x 8.05"
 
 print(PCA_Plot)
